dc6f36a9447e5d9d6cc71ba6f9f5a0fbbd2fd330
Umesh Rai <UmeshRai508@outlook.com>
2020-06-03 00:25:51 +0100

    Initial Move

---
+++ data.js
@@ -0,0 +1,312 @@
+units=[
+	{
+		"unit": "1 Rifles",
+		"address": "Beachley Barracks, Beachley Road, CHEPSTOW, Wales, NP16 7YG",
+		"badge_url": "https://www.army.mod.uk/media/1070/rifles-grad.png?center=0.58860759493670889,0.56329113924050633&mode=crop&width=200&height=200&rnd=131740643320000000\r"
+	},
+	{
+		"unit": "103th Regiment Royal Artillery",
+		"address": "RHQ, Jubilee Barracks, Prescot Road, St Helens, WA10 3UB",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "104th Regiment Royal Artillery",
+		"address": "RHQ, Raglan Barracks, Newport, South Wales, NP20 5XE",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "105th Regiment Royal Artillery",
+		"address": "RHQ, Artillery House, 301 Colinton Road, Edinburgh, EH13 0LA",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "106th (Yeomanry) Regiment Royal Artillery",
+		"address": "Napier House, Grove Park, London, SE12 0BH",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "11th Infantry Brigade & HQ South East",
+		"address": "Roebuck House, Cavans Road, Aldershot, GU11 2LQ",
+		"badge_url": "https://www.army.mod.uk/media/4466/11_inf_x_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131788212320000000\r"
+	},
+	{
+		"unit": "12th Regiment Royal Artillery",
+		"address": "Baker Barracks Thorney Island Emsworth HAMPSHIRE PO10 8DH",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "14th Regiment Royal Artillery",
+		"address": "14th Regiment Royal Artillery, Royal Artillery Barracks, Larkhill, SALISBURY, Wiltshire, SP4 8QT",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "152 Regiment",
+		"address": "Palace Barracks, Holywood BT18 9RA",
+		"badge_url": "https://www.army.mod.uk/media/3742/rlc_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131740650630000000\r"
+	},
+	{
+		"unit": "156 Regiment",
+		"address": "Army Reserve Centre, Mather Ave, Liverpool L18 6HF",
+		"badge_url": "https://www.army.mod.uk/media/3742/rlc_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131740650630000000\r"
+	},
+	{
+		"unit": "16 Air Assault Brigade",
+		"address": "8 Circular Road South CO2 7UT",
+		"badge_url": "https://www.army.mod.uk/media/1055/16aa_200.jpg?anchor=center&mode=crop&width=200&height=200&rnd=131740647330000000\r"
+	},
+	{
+		"unit": "162 Regiment",
+		"address": "162 Regiment RLC, 31 Wigman Road, Bilborough, Nottingham, NG8 3 HY",
+		"badge_url": "https://www.army.mod.uk/media/3742/rlc_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131740650630000000\r"
+	},
+	{
+		"unit": "165 Port and Maritime Regiment",
+		"address": "Derriford Army Reserve Centre, PLYMOUTH, Devon, PL5EW",
+		"badge_url": "https://www.army.mod.uk/media/3742/rlc_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131740650630000000\r"
+	},
+	{
+		"unit": "16th Regiment Royal Artillery",
+		"address": "Baker Barracks, Thorney Island, Emsworth, West Sussex, PO10 8DH",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "19th Regiment Royal Artillery",
+		"address": "Bhurtpore Barracks, Tidworth, Hants, SP9 7AS",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "1st Regiment Royal Horse Artillery",
+		"address": "Assaye Barracks Tidworth Hampshire SP9 7AB",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "1st The Queens Dragoon Guards",
+		"address": "1 Worthing Rd, Swanton Morley, Dereham, NR20 4QD",
+		"badge_url": "https://www.army.mod.uk/media/4390/qdg_badge200.png?anchor=center&mode=crop&width=200&height=200&rnd=131786487710000000\r"
+	},
+	{
+		"unit": "2 Operational Support Group RLC",
+		"address": "Prince William of Gloucester Barracks, Grantham, NG31 7TJ",
+		"badge_url": "https://www.army.mod.uk/media/5244/qoglr_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131891696310000000\r"
+	},
+	{
+		"unit": "2 RIFLES",
+		"address": "Thiepval Barracks, Magheralave Road, LISBURN, Northern Ireland, BFPO 801",
+		"badge_url": "https://www.army.mod.uk/media/1070/rifles-grad.png?center=0.58860759493670889,0.56329113924050633&mode=crop&width=200&height=200&rnd=131740643320000000\r"
+	},
+	{
+		"unit": "204 Field Hospital",
+		"address": "Hydebank Army Reserve Centre, Hospital Road, Belfast BT8 8JP",
+		"badge_url": "https://www.army.mod.uk/media/4257/204_fd_hosp200.png?anchor=center&mode=crop&width=200&height=200&rnd=131769079560000000\r"
+	},
+	{
+		"unit": "205 Field Hospital",
+		"address": "Graham House, 130 Whitefield Road, Glasgow G51 2YE",
+		"badge_url": "https://www.army.mod.uk/media/4229/ramc_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131765637960000000\r"
+	},
+	{
+		"unit": "21 Engineer Regiment",
+		"address": "21 Engineer Regiment, Claro Baracks, Chatham Road, Ripon, North Yorkshire, HG4 2RD",
+		"badge_url": "https://www.army.mod.uk/media/4387/re_badge200.png?anchor=center&mode=crop&width=200&height=200&rnd=131988500250000000\r"
+	},
+	{
+		"unit": "253 (North Irish) Medical Regiment",
+		"address": "Army Reserve Centre, Sunnyside Street, Belfast BT7 3DA",
+		"badge_url": "https://www.army.mod.uk/media/4198/253_med_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131765556780000000\r"
+	},
+	{
+		"unit": "26th Regiment Royal Artillery",
+		"address": "Purvis Lines, Larkhill",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "29th Commando Regiment Royal Artillery",
+		"address": "The Royal Citadel, Plymouth, Devon, PL1 2PD",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "3 Medical Regiment",
+		"address": "Lancaster House, Devonshire Road, Chorley, PR7 2DJ",
+		"badge_url": "https://www.army.mod.uk/media/4227/3_med_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131765633300000000\r"
+	},
+	{
+		"unit": "3 RIFLES",
+		"address": "Redford Barracks, Redford Road, EDINBURGH, Midlothian, EH13 9QW",
+		"badge_url": "https://www.army.mod.uk/media/1070/rifles-grad.png?center=0.58860759493670889,0.56329113924050633&mode=crop&width=200&height=200&rnd=131740643320000000\r"
+	},
+	{
+		"unit": "32nd Regiment Royal Artillery",
+		"address": "Roberts Barracks, Larkhill, Salisbury, SP4 8QU",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "383 Commando Petroleum Troop",
+		"address": "Army Reserve Centre, Brest Road, Derriford, Plymouth, PL6 5EW",
+		"badge_url": "https://www.army.mod.uk/media/3742/rlc_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131740650630000000\r"
+	},
+	{
+		"unit": "3rd Regiment Royal Horse Artillery",
+		"address": "Albemarle Barracks Nr Harlow Hill Newcastle-Upon-Tyne NE15 0RF",
+		"badge_url": "https://www.army.mod.uk/media/5109/3rha_grad-200.png?anchor=center&mode=crop&width=200&height=200&rnd=131866797060000000\r"
+	},
+	{
+		"unit": "4 RIFLES",
+		"address": "New Normandy Barracks, Evelyn Woods Road, ALDERSHOT, Hampshire, GU11 2LZ",
+		"badge_url": "https://www.army.mod.uk/media/1070/rifles-grad.png?center=0.58860759493670889,0.56329113924050633&mode=crop&width=200&height=200&rnd=131740643320000000\r"
+	},
+	{
+		"unit": "4 YORKS",
+		"address": "Worsley Barracks, Fulford Road, York, YO10 4EB",
+		"badge_url": "https://www.army.mod.uk/media/4120/yorks_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131758857400000000\r"
+	},
+	{
+		"unit": "42 Engineer Regiment",
+		"address": "RAF Wyton, Huntingdon, Cambridgeshire, PE28 2EA",
+		"badge_url": "https://www.army.mod.uk/media/4387/re_badge200.png?anchor=center&mode=crop&width=200&height=200&rnd=131988500250000000\r"
+	},
+	{
+		"unit": "47th Regiment Royal Artillery",
+		"address": "Horne Barracks, Larkhill, Salisbury, Wiltshire SP4 8QE",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "4th Regiment Royal Artillery",
+		"address": "Alanbrooke Barracks, Topcliffe, Thirsk, North Yorkshire, YO7 3EY",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "5 Military Intelligence",
+		"address": "Redford Barracks, EDINBURGH, EH13 0LA",
+		"badge_url": "https://www.army.mod.uk/media/4631/int_5mi_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131807150430000000\r"
+	},
+	{
+		"unit": "5 RIFLES",
+		"address": "Ward Barracks,Bulford Station, SALISBURY, Wiltshire, SP4 9NA",
+		"badge_url": "https://www.army.mod.uk/media/1070/rifles-grad.png?center=0.58860759493670889,0.56329113924050633&mode=crop&width=200&height=200&rnd=131740643320000000\r"
+	},
+	{
+		"unit": "5th Regiment Royal Artillery",
+		"address": "Marne Barracks, Catterick, Richmond North Yorkshire DL10 7NP",
+		"badge_url": "https://www.army.mod.uk/media/4080/ra_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756122860000000\r"
+	},
+	{
+		"unit": "6 Aberdeen UOTC",
+		"address": "Aberdeen Universities� Officer Training Corps, Gordon Barracks, Ellon Road, Bridge of Don, Aberdeen, AB23 8DB",
+		"badge_url": "https://www.army.mod.uk/media/4241/rmas_grad_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131765712890000000\r"
+	},
+	{
+		"unit": "6 RIFLES",
+		"address": "St Andrews Road, BISHOPS AUCKLAND, DL14 6RX",
+		"badge_url": "https://www.army.mod.uk/media/1070/rifles-grad.png?center=0.58860759493670889,0.56329113924050633&mode=crop&width=200&height=200&rnd=131740643320000000\r"
+	},
+	{
+		"unit": "7 Military Intelligence",
+		"address": "Army Reserves Centre, Dorian Road, Bristol , BS7 0XL",
+		"badge_url": "https://www.army.mod.uk/media/4637/int_7mi_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131807152200000000\r"
+	},
+	{
+		"unit": "7 RIFLES",
+		"address": "Battalion Headquarters, Brock Barracks, Oxford Road, READING, RG30 1HW",
+		"badge_url": "https://www.army.mod.uk/media/1070/rifles-grad.png?center=0.58860759493670889,0.56329113924050633&mode=crop&width=200&height=200&rnd=131740643320000000\r"
+	},
+	{
+		"unit": "77th Brigade",
+		"address": "Denison Barracks, Hermitage, Thatcham, RG19TP",
+		"badge_url": "https://www.army.mod.uk/media/4464/77x_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131788193150000000\r"
+	},
+	{
+		"unit": "7th Infantry Brigade and HQ East",
+		"address": "Kendrew Barracks, Cottesmore, Rutland, LE15 7BL",
+		"badge_url": "https://www.army.mod.uk/media/4470/7x_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131788216590000000\r"
+	},
+	{
+		"unit": "7th Parachute Regiment Royal Horse Artillery",
+		"address": "Merville Barracks, Circular Road South, Colchester, Essex CO2 7UT",
+		"badge_url": "https://www.army.mod.uk/media/4083/rha_badge_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131756123040000000\r"
+	},
+	{
+		"unit": "6 Armoured Close Support Battalion",
+		"address": "Armoured Close Support Battalion REME, Delhi Barracks, Tidworth, WILTSHIRE, SP9 7DX",
+		"badge_url": "https://www.army.mod.uk/media/4404/reme_badge200.png?anchor=center&mode=crop&width=200&height=200&rnd=131786518360000000\r"
+	},
+	{
+		"unit": "Army Air Corps",
+		"address": "Middle Wallop, Stockbridge, SO20 8DY",
+		"badge_url": "https://www.army.mod.uk/media/4402/aac_badge200.png?anchor=center&mode=crop&width=200&height=200&rnd=131872714730000000\r"
+	},
+	{
+		"unit": "Birmingham UOTC",
+		"address": "Birmingham UOTC, Montgomery House, Stoney Lane, Sparkbrook, Birmingham, B21 8AT",
+		"badge_url": "https://www.army.mod.uk/media/4241/rmas_grad_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131765712890000000\r"
+	},
+	{
+		"unit": "Brigade of Gurkhas",
+		"address": "Headquarters Brigade of Gurkhas, Robertson House, Camberley, GU15 4PQ",
+		"badge_url": "https://www.army.mod.uk/media/4455/bde_gurkha200.png?anchor=center&mode=crop&width=200&height=200&rnd=131788181040000000\r"
+	},
+	{
+		"unit": "Bristol UOTC",
+		"address": "Bristol University Officers Training Corps, The Artillery Grounds, Whiteladies Road, Bristol, BS2LG",
+		"badge_url": "https://www.army.mod.uk/media/4241/rmas_grad_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131765712890000000\r"
+	},
+	{
+		"unit": "British Army Jazz Orchestra",
+		"address": "Kneller Hall, Twickenham, Middlesex, TW2 7DU",
+		"badge_url": "https://www.army.mod.uk/media/4409/camus_badge200.png?anchor=center&mode=crop&width=200&height=200&rnd=131786528860000000\r"
+	},
+	{
+		"unit": "Cambridge UOTC",
+		"address": "Army Reserve Centre , Coldhams Lane, Cambridge, CB1 3HS",
+		"badge_url": "https://www.army.mod.uk/media/4241/rmas_grad_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131765712890000000\r"
+	},
+	{
+		"unit": "Coldstream Guards",
+		"address": "Victoria Barracks, Sheet St, Windsor SL4 1HF",
+		"badge_url": "https://www.army.mod.uk/media/4502/cldstrmgds_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131789056760000000\r"
+	},
+	{
+		"unit": "Corps of Army Music",
+		"address": "Kneller Hall, Twickenham, Middlesex, TW2 7DU",
+		"badge_url": "https://www.army.mod.uk/media/4409/camus_badge200.png?anchor=center&mode=crop&width=200&height=200&rnd=131786528860000000\r"
+	},
+	{
+		"unit": "Corps of Royal Electrical and Mechanical Engineers",
+		"address": "RHQ REME, The Prince Philip Barracks, Lyneham, Chippenham, SN15 4XX",
+		"badge_url": "https://www.army.mod.uk/media/4404/reme_badge200.png?anchor=center&mode=crop&width=200&height=200&rnd=131786518360000000\r"
+	},
+	{
+		"unit": "Duke of Lancasters Regiment",
+		"address": "Fulwood Barracks Preston PR2 8AA",
+		"badge_url": "https://www.army.mod.uk/media/4129/lancs_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131758874290000000\r"
+	},
+	{
+		"unit": "East Midlands UOTC",
+		"address": "East Midlands UOTC, Army Reserve Centre, Broadgate, Beeston, Nottingham, NG9 2HF",
+		"badge_url": "https://www.army.mod.uk/media/4241/rmas_grad_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131765712890000000\r"
+	},
+	{
+		"unit": "Edinburgh UOTC",
+		"address": "City of Edinburgh UOTC, 301 Colinton Road, Edinburgh, EH13 0LA",
+		"badge_url": "https://www.army.mod.uk/media/4241/rmas_grad_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131765712890000000\r"
+	},
+	{
+		"unit": "Exeter UOTC",
+		"address": "Exeter UOTC, Wyvern Barracks, Exeter, Devon, EX2 6AR",
+		"badge_url": "https://www.army.mod.uk/media/4241/rmas_grad_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131765712890000000\r"
+	},
+	{
+		"unit": "Glasgow and Strathclyde UOTC",
+		"address": "Glasgow and Strathclyde UOTC, 95 University Place, Glasgow G12 8SU",
+		"badge_url": "https://www.army.mod.uk/media/4241/rmas_grad_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131765712890000000\r"
+	},
+	{
+		"unit": "Household Cavalry",
+		"address": "Hyde Park Barracks, Knightsbridge, London, SW7 1SE",
+		"badge_url": "https://www.army.mod.uk/media/4389/hcr_badge200.png?anchor=center&mode=crop&width=200&height=200&rnd=131786486020000000\r"
+	},
+	{
+		"unit": "HQ 11th Signal and West Midlands Brigade",
+		"address": "HQ 11 Sig & WM Brigade Venning Barracks Donnington, TELFORD TF2 8AF",
+		"badge_url": "https://www.army.mod.uk/media/3663/11sig_200.png?anchor=center&mode=crop&width=200&height=200&rnd=131740655800000000"
+	}
+]
